d6d11500a746b1a3455aa9aeb9f130a640ce1401
Rafaella Neofytou <rafaella@think-engineer.com>
2019-10-01 16:11:25 +0100

    Doxygen comments etc

--- OPT3001.cpp
+++ OPT3001.cpp
@@ -1,49 +1,95 @@
+/**
+  * @file    OPT3001.cpp
+  * @version 1.0.0
+  * @author  
+  * @brief   OPT3001 driver module. Measures light intensity in lux.
+  * 
+  * Example: 
+  * code
+  * #include "mbed.h"
+  * #include "main.h"
+  * #include "OPT3001/OPT3001.h"
+  *
+  * HDC1080 HDC1080(SDA,SCL); 
+  *  
+  * int main() {
+  * float temp_C =  HDC1080.ReadTemperature();  
+  * }
+  * @endcode
+  */
 
+/** Includes
+ */
 #include "OPT3001.h"
-#include "mbed.h"
 
- //Shift by one bit to get 7 bit I2C Addrress
-//char HDC_COMMN = HDC_MANID_OFF;
+
 const float OPT_CHIP_ERROR = -255;
-const unsigned long OPT_CHIP_SER_ERROR = 0;
-char Buffer1[5];
-Serial pc(PC_10, PC_11);
+char data[5];
 
+/** Constructor.
+ * 
+ * @param sda I2C data line pin
+ * @param scl I2C clock line pin
+ */
 OPT3001::OPT3001(PinName sda, PinName slc) : I2C(sda,slc) 
 {
-    memset(Buffer1,'\0',5);
-    Buffer1[0] = OPT3001_REG_CONFIG;
-    this->frequency(I2C_FREQ);
-    int res = this->write(OPT3001_DEFAULT_ADDRESS, Buffer1, 2);
-    //pc.printf("OPT Constructor Initialization  : Res =%d\r\n", res);
+    memset(data,'\0',5);
+    data[0] = OPT3001_REG_CONFIG;
+    //this->frequency(I2C_FREQ);
+    int res = this->write(OPT3001_DEFAULT_ADDRESS, data, 2);
+    
 }
-
+/* Initialisation. 
+*  Reads The Manufacturer ID on register address 0x7E.
+*  @return Manufacturer_ID 
+*/
 int OPT3001::ReadSignature(void)
 { 
     
 uint16_t  Manufacturer_ID = read2Bytes(OPT3001_DEFAULT_ADDRESS,OPT3001_REG_MAN_ID);
     if (Manufacturer_ID == 0) {
      
-      
-        pc.printf("Error  reading HDC Manufacturer ID\r\n");
         return (int) OPT_CHIP_ERROR;
     } else {  
-           
-     
-        pc.printf("Manufacturer_ID  :%x\r\n", (int) Manufacturer_ID);
+        
         return Manufacturer_ID;
     }    
 }  
-    
+
+    unsigned long OPT3001::ReadDeviceId(void)
+{    
+    uint16_t  Device_ID = read2Bytes(OPT3001_DEFAULT_ADDRESS, OPT3001_REG_DEV_ID);
+    if (Device_ID == 0) {
+
+        return (int) OPT_CHIP_ERROR;
+    } else {  
+           
+        return Device_ID;
+    } 
+}
+/** Ambient light measurement TI OPT3001
+ */   
  float OPT3001::Readlight()
-{  
-    uint16_t  rawT = read2Bytes(OPT3001_DEFAULT_ADDRESS, OPT3001_REG_RESULT);
-    // if (rawT == 0) {
-    //      pc.printf("Error reading");
-    //     return OPT_CHIP_ERROR;
-    // } else {
-        //     /* swap bytes, as OPT3001 sends MSB first and I2C driver expects LSB first */
-       
+{   
+        /* start measurement
+        OPT3001 needs this extra initialisation before acquiring the results- 
+        (zero to full-scale) */
+    
+        data[0] =OPT3001_REG_CONFIG;
+        data[1] =OPT3001_CFG_DEFAULT_1;
+        data[2]= OPT3001_CFG_DEFAULT;
+    
+        this->write(OPT3001_DEFAULT_ADDRESS, data, 3);
+        wait_us(1500000); //15ms
+
+        uint16_t  rawT = read2Bytes(OPT3001_DEFAULT_ADDRESS, OPT3001_REG_RESULT);
+    
+        if (rawT==OPT_CHIP_ERROR) {
+
+            return (int) OPT_CHIP_ERROR;
+        }
+         else{
+        /* swap bytes, as OPT3001 sends MSB first and I2C driver expects LSB first */
         rawT = (rawT << 8) | (rawT >> 8);
 
         /* calculate lux per LSB */
@@ -54,72 +100,37 @@ uint16_t  Manufacturer_ID = read2Bytes(OPT3001_DEFAULT_ADDRESS,OPT3001_REG_MAN_I
         rawT &= 0x0FFF;
         float lght_lux= (((int)rawT * lsb_size_x100) / 100);
 
-       
-  
-        return lght_lux;
-    //}
-}   
     
-
-    unsigned long OPT3001::ReadSerialNumber(void)
-{    
-    wait_us(15000); //15ms
-    memset(Buffer1,0,4);
-    Buffer1[0] = OPT3001_REG_MAN_ID;
-    int res = this->write(OPT3001_DEFAULT_ADDRESS, Buffer1, 1);
-    if (res != 0) {      
-     
-    
-      return (unsigned long) OPT_CHIP_SER_ERROR;
-    }      
- 
-    wait_us(15000); //0.015
-    memset(Buffer1,0,4);
-    res = this->read(OPT3001_DEFAULT_ADDRESS, Buffer1,4);
-    if (res != 0) {
-     
-    
-     
-      return (unsigned long) OPT_CHIP_SER_ERROR;
+        return lght_lux; 
     }
+}   
       
-//    unsigned long rawser = Buffer[0] << 16 | Buffer[1] << 8 | Buffer[0];
-      unsigned long rawser = Buffer1[2] << 16 | Buffer1[1] << 8 | Buffer1[0];
-  
-   
-   
-    return rawser;
-}
-    
-    
     //Private Member functions 
     
     uint16_t OPT3001::read2Bytes(int chip_addr, int offset)
 {
-    memset(Buffer1,0,3);
+    memset(data,0,3);
     // send chip address onto buss
-    Buffer1[0] = offset;
-    int res =this->write(chip_addr, Buffer1, 1);
+    data[0] = offset;
+    int res =this->write(chip_addr, data, 1);
     if (res != 0) {
-       
-    pc.printf("HERE");
-       
-        return 0;
+             
+        return OPT_CHIP_ERROR;
     }
     // read data from chip
     wait_us(15000); //0.015
-    memset(Buffer1,0,3);
-    res = this->read(chip_addr, Buffer1,2);
+    memset(data,0,3);
+    res = this->read(chip_addr, data,2);
     if (res != 0) {
-    pc.printf("THERE");
-        return 0;
+   
+        return OPT_CHIP_ERROR;
     }
     
 
-    uint16_t result = ((((uint16_t)Buffer1[0]) << 8) | Buffer1[1]) & 0x1FFF;
-    pc.printf("Buffer Result   : %d\r\n", result);
+    // uint16_t result = ((((uint16_t)Buffer1[0]) << 8) | Buffer1[1]) & 0x1FFF;
+    // pc.printf("Buffer Result   : %d\r\n", result);
 
-    return  Buffer1[0] << 8 | Buffer1[1];
+    return  data[0] << 8 | data[1];
 }
 
 
--- OPT3001.h
+++ OPT3001.h
@@ -1,22 +1,58 @@
 /**
- bla bla
- */
+  * @file    OPT3001.h
+  * @version 1.0.0
+  * @author  
+  * @brief   Header file of the OPT3001 driver module
+  * 
+  * Example: 
+  * code
+  * #include "mbed.h"
+  * #include "main.h"
+  * #include "OPT3001/OPT3001.h"
+  *
+  * OPT3001 OPT3001(SDA,SCL); 
+  *  
+  * int main() {
+  * float lght_lx =  HDC1080.Readlight();  
+  * }
+  * @endcode
+  */
 
+/** Define to prevent recursive inclusion
+ */
+#pragma once
+
+/** Includes
+ */
 #include "mbed.h"
 
 #ifndef MBED_OPT3001_H
 #define MBED_OPT3001_H
 
-#define     I2C_FREQ                            100000
+/** Uncomment if needed
+*/
+//#define     I2C_FREQ                            100000
 
-// default I2C address with address pin tied to GND
-#define OPT3001_DEFAULT_ADDRESS                 (0x44 << 1) //10001000?
+
+/** Register Map.
+ *  Device ID Register (offset = 7Fh) [reset = 3001h]
+ *  Manufacturer ID Register (offset = 7Eh) [reset = 5449h]
+ */
 #define OPT3001_REG_RESULT                      0x00 
-#define OPT3001_REG_CONFIG                      0x01 
+#define OPT3001_REG_CONFIG                      0x01
 #define OPT3001_REG_MAN_ID                      0x7E //manifucture id
-#define OPT3001_REG_CHIP_ID                     0x7F //read device id
-#define OPT3001_MAN_ID                          0x5449 //address of manifucture id 
+#define OPT3001_REG_DEV_ID                      0x7F //read device id
 
+/** Default HDC1080 I2C address. This should be set according to the 
+ *  configuration of the hardware address pins. 
+ */
+#define OPT3001_DEFAULT_ADDRESS                 (0x44 << 1) 
+/** Configure Register changed for calibrating the device,
+ * Default values (offset = 01h) [reset = C810h]- for full scale lux measurement
+ * Changed to (offset = 01h) [reset = AE0Ch]
+ */
+#define OPT3001_CFG_DEFAULT_1                   0xAE
+#define OPT3001_CFG_DEFAULT                     0x0C
 
 class OPT3001:public I2C {
 public:
@@ -27,11 +63,11 @@ public:
     
     int ReadSignature(void);
     float Readlight( void);                                
-    unsigned long ReadSerialNumber(void);            
+    unsigned long ReadDeviceId(void);            
     
 protected:
      uint16_t read2Bytes(int chip_addr, int offset);
 };
 
-#endif // MBED_OPT3001_H
+#endif 
 
